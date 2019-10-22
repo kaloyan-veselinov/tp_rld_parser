d7afc0f64ec3db018ab7129aa6c6f3a0e9688cee
Kaloyan Veselinov <kvesselinov@gmail.com>
2019-10-22 10:46:07 +0200

    Usine à GW et à Mesure

--- parser.py
+++ parser.py
@@ -14,8 +14,8 @@ class Gateway:
 
 
 class Mesure:
-    def __init__(self, device_id: str, data_rate: str, coding_rate: str, latitude: float, longitude: float, temperature: int, humidity: int, gateways: List[Gateway]):
-        self.device_id = device_id
+    def __init__(self, data_rate: str, coding_rate: str, latitude: float, longitude: float,
+                 temperature: int, humidity: int, gateways: List[Gateway]):
         self.data_rate = data_rate
         self.coding_rate = coding_rate
         self.latitude = latitude
@@ -25,12 +25,34 @@ class Mesure:
         self.gateways = gateways
 
 
-def get_data_from_line(line: str)->str:
+def parse_gateway_from_json(gateway_data: JSONObject) -> Gateway:
+    return Gateway(
+        id=gateway_data['gtw_id'],
+        latitude=gateway_data['latitude'],
+        longitude=gateway_data['longitude'],
+        rssi=gateway_data['rssi'],
+        snr=gateway_data['snr']
+    )
+
+
+def parse_mesure_from_json(mesure_data: JSONObject) -> Mesure :
+    return Mesure(
+        data_rate=mesure_data['metadata']['data_rate'],
+        coding_rate=mesure_data['metadata']['coding_rate'],
+        latitude=mesure_data['payload_fields']['latitude'],
+        longitude=mesure_data['payload_fields']['longitude'],
+        temperature=mesure_data['payload_fields']['latitude'],
+        humidity=mesure_data['payload_fields']['humidity'],
+        gateways=[parse_gateway_from_json(g) for g in mesure_data['metadata']['gateways']]
+    )
+
+
+def get_data_from_line(line: str) -> str:
     arr: List[str] = line.split(" ")
     return arr[1]
 
 
-def parse_line_to_json(line: str)->JSONObject:
+def parse_line_to_json(line: str) -> JSONObject:
     data_str: str = get_data_from_line(line)
     return json.loads(data_str)
 
