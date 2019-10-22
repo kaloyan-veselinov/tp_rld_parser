8b85f164b4a75325d8988a7ef09e18e18852ab97
Kaloyan Veselinov <kvesselinov@gmail.com>
2019-10-22 09:48:26 +0200

    Print ligne par ligne

--- parser.py
+++ parser.py
@@ -3,5 +3,6 @@ import sys
 if __name__ == "__main__":
     path = sys.argv[1]
     with open(path) as file:
-        content = file.readlines()
-        print(content)
+        content = [l.strip() for l in file.readlines()]
+        for line in content:
+            print(line)
